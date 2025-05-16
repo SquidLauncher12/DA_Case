57e78e2e0ee179291975761ac1ff10f92d1833b6
Jo Bo <squidlauncher12@gmail.com>
2025-05-16 06:55:49 +0000

    due to unclear instructions my code was a little different and i wanted it to look like the tutorial, so i copied their code in order to continue

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,6 +1,30 @@
-select 
-    order_id,
-    customer_id,
-    stripe.amount
-from {{ref('stg_jaffle_shop__orders')}} as orders
-LEFT JOIN {{ref('stg_stripe__payments')}} as stripe ON stripe.orderid = orders.order_id 
+with orders as  (
+    select * from {{ ref ('stg_jaffle_shop__orders' )}}
+),
+
+payments as (
+    select * from {{ ref ('stg_stripe__payments') }}
+),
+
+order_payments as (
+    select
+        order_id,
+        sum (case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
+),
+
+ final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce (order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
+)
+
+select * from final
--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,9 +1,11 @@
-SELECT 
-id as payment_id,
-orderid as order_id,
-paymentmethod as payment_method,
-status,
---store amount as dollars
-amount/100 as amount,
-created as created_at
-FROM {{ source('stripe', 'payment') }}
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
+
+from {{ source('stripe', 'payment') }}
